55e33e398580d2e7d37cfade8f4062d3d6dea917
Filip Kubat <filip.kubat@wp.pl>
2018-05-08 00:28:04 +0200

    repair chosen plugin in other subsites, correct redirect do kanban after adding task

--- ManageOnline/Controllers/TaskController.cs
+++ ManageOnline/Controllers/TaskController.cs
@@ -27,7 +27,7 @@ namespace ManageOnline.Controllers
             int ProjectId = task.ProjectId;
             using (DbContextModel db = new DbContextModel())
             {
-                
+
                 task.TaskCreationDate = DateTime.Now;
                 task.Project = db.Projects.Where(x => x.ProjectId.Equals(ProjectId)).FirstOrDefault();
                 task.UserWhoAddTask = db.UserAccounts.Where(x => x.UserId.Equals(userIdInt)).FirstOrDefault();
@@ -36,10 +36,10 @@ namespace ManageOnline.Controllers
                 db.Tasks.AddOrUpdate(task);
                 db.SaveChanges();
             }
-           return RedirectToAction("KanbanBoard", "ProjectPanel", ProjectId);
+            return RedirectToAction("KanbanBoard", "ProjectPanel", new { projectId = ProjectId });
         }
 
-        public ActionResult UpdateTaskPosition(string column1,string column2, string column3)
+        public ActionResult UpdateTaskPosition(string column1, string column2, string column3)
         {
             var column1Tasks = column1.Split(",".ToCharArray(), StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
             var column2Tasks = column2.Split(",".ToCharArray(), StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
@@ -59,7 +59,7 @@ namespace ManageOnline.Controllers
                 int counter2 = 1;
                 foreach (var itemId in column2Tasks)
                 {
-  
+
                     TaskModel task = db.Tasks.Where(x => x.TaskId.Equals(itemId)).FirstOrDefault();
                     task.RowNumber = counter2;
                     task.ColumnNumber = 2;
