a7a601517160e6ce594d9bd6e01ea8dfe60c74ff
Filip Kubat <filip.kubat@wp.pl>
2018-06-10 14:13:47 +0200

    messages completed mechanism

--- ManageOnline/Controllers/MessagesController.cs
+++ ManageOnline/Controllers/MessagesController.cs
@@ -1,6 +1,7 @@
 ﻿using ManageOnline.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -10,6 +11,40 @@ namespace ManageOnline.Controllers
     public class MessagesController : Controller
     {
         //GET: Messages
+
+        public ActionResult MessagesInfo(int userId)
+        {
+            MessageModel message = new MessageModel();
+            using (DbContextModel db = new DbContextModel())
+            {
+                ICollection<MessageModel> messages = db.Messages
+                                                    .Include("Receiver")
+                                                    .Include("Sender")
+                                                    .Where(x => x.Receiver.UserId.Equals(userId)).ToList();
+
+                ViewBag.messageNotSeenCount = messages.Where(x => x.IsSeen == false).Count();
+                
+                return PartialView("_messagesInfo");
+            }
+            
+        }
+
+        public ActionResult ShowMessageDetails(int messageId)
+        {
+            using (DbContextModel db = new DbContextModel())
+            {
+                var message = db.Messages.Include("Receiver")
+                                         .Include("Sender")
+                                         .Where(x => x.MessageId.Equals(messageId)).FirstOrDefault();
+
+                if (!message.IsSeen)
+                    message.IsSeen = true;
+                db.Entry(message).State = EntityState.Modified;
+                db.SaveChanges();
+                return PartialView("_messageDetails", message);
+            }
+
+        }
         public ActionResult SendMessage(int userId)
         {
             MessageModel message = new MessageModel();
@@ -30,16 +65,9 @@ namespace ManageOnline.Controllers
                 message.Receiver = db.UserAccounts.Where(x => x.UserId.Equals(receiverUserId)).FirstOrDefault();
                 message.Sender = db.UserAccounts.Where(x => x.UserId.Equals(senderIdInt)).FirstOrDefault();
                 message.DateSend = DateTime.Now;
+                message.IsSeen = false;
                 db.Messages.Add(message);
-                try
-                {
                     db.SaveChanges();
-                }
-                catch (Exception ex)
-                {
-                    var ble = ex;
-                }
-
             }
 
             return RedirectToAction("ShowSendedMessages");
@@ -49,11 +77,12 @@ namespace ManageOnline.Controllers
         {
             using (DbContextModel db = new DbContextModel())
             {
+                int userIdInt = Convert.ToInt32(System.Web.HttpContext.Current.Session["UserId"]);
                 ICollection<MessageModel> messages = db.Messages
                                                     .Include("Receiver")
                                                     .Include("Sender")
-                                                    .Where(x => x.Receiver.UserId.Equals(System.Web.HttpContext.Current.Session["UserId"])).ToList();
-                return View();
+                                                    .Where(x => x.Receiver.UserId.Equals(userIdInt)).ToList();
+                return View(messages);
             }
         }
 
@@ -67,7 +96,7 @@ namespace ManageOnline.Controllers
                                                     .Include("Receiver")
                                                     .Include("Sender")
                                                     .Where(x => x.Sender.UserId.Equals(senderIdInt)).ToList();
-                return View();
+                return View(messages);
             }
         }
 
