94262ea5fc69f247e2350acf6c5b9a108a5a2c39
Filip Kubat <filip.kubat@wp.pl>
2018-06-11 14:58:56 +0200

    edit view employees list, and search by average rate possibility

--- ManageOnline/Controllers/EmployeesController.cs
+++ ManageOnline/Controllers/EmployeesController.cs
@@ -12,11 +12,109 @@ namespace ManageOnline.Controllers
         // GET: Employees
         public ActionResult EmployeesList()
         {
-            DbContextModel db = new DbContextModel();
+            using (DbContextModel db = new DbContextModel())
+            {
+                var workersList = db.UserAccounts.Where(u => u.Role == Roles.Pracownik).ToList();
 
-            var workersList = db.UserAccounts.Where(u => u.Role == Roles.Pracownik);
+                var skills = db.Skills.ToList();
+                MultiSelectList skillsList = new MultiSelectList(skills, "SkillId", "SkillName");
 
-            return View(workersList);
+                if ((TempData["Skills"] as MultiSelectList) != null)
+                {
+                    var selectedSkillsSelectList = TempData["Skills"] as MultiSelectList;
+                    var selectedSkills = selectedSkillsSelectList.Items;
+                }
+
+                TempData["Skills"] = skillsList;
+
+                foreach (var employee in workersList)
+                {
+                    if (employee.Skills != null)
+                    {
+                        employee.SkillsArray = employee.Skills.Split(',').ToArray();
+                        foreach (var skillId in employee.SkillsArray)
+                        {
+                            var skillIdInt = Convert.ToInt32(skillId);
+                            var skill = db.Skills.Where(x => x.SkillId.Equals(skillIdInt)).FirstOrDefault();
+                            employee.SkillsCollection.Add(skill);
+                        }
+                    }
+                }
+
+                return View(workersList);
+            }
+
+        }
+
+        [HttpPost]
+        public ActionResult EmployeesList(FormCollection form)
+        {
+            using (DbContextModel db = new DbContextModel())
+            {
+
+                var workersList = db.UserAccounts.Where(u => u.Role == Roles.Pracownik).ToList();
+
+                var skills = db.Skills.ToList();
+                MultiSelectList skillsList = new MultiSelectList(skills, "SkillId", "SkillName");
+
+                TempData["Skills"] = skillsList;
+
+                foreach (var employee in workersList)
+                {
+                    if (employee.Skills != null)
+                    {
+                        employee.SkillsArray = employee.Skills.Split(',').ToArray();
+                        foreach (var skillId in employee.SkillsArray)
+                        {
+                            var skillIdInt = Convert.ToInt32(skillId);
+                            var skill = db.Skills.Where(x => x.SkillId.Equals(skillIdInt)).FirstOrDefault();
+                            employee.SkillsCollection.Add(skill);
+                        }
+                    }
+                }
+                List<UserBasicModel> filteredDataContext = new List<UserBasicModel>();
+                string rateValue= form["rateValueText"];
+                string rateValuePreparedToDoubleConvert = rateValue.Replace(".", ",");
+                double minimumAverageRate = Convert.ToDouble(rateValuePreparedToDoubleConvert);
+                if (form["Skills"] != null || form["rateValueText"] != null)
+                {
+                    if (form["Skills"] != null)
+                    {
+                        var selectedSkills = form["Skills"];
+                        var selectedSkillsArray = selectedSkills.Split(',').ToArray();
+                        int countOfSelectedSkills = selectedSkillsArray.Count();
+
+                        foreach (var employee in workersList)
+                        {
+                            bool flag = false;
+                            for (int i = 0; i < countOfSelectedSkills; i++)
+                            {
+                                if (employee.SkillsArray.Contains(selectedSkillsArray[i]))
+                                    flag = true;
+                                else
+                                    flag = false;
+                            }
+                            if (flag)
+                                filteredDataContext.Add(employee);
+                        }
+                        if (form["rateValueText"] != null)
+                        {
+                            var filteredDataContextWithCheckedAverageRateAndSkills = filteredDataContext.Where(x => x.AverageRate >= minimumAverageRate).ToList();
+                            return View(filteredDataContextWithCheckedAverageRateAndSkills);
+                        }
+                        else
+                        {
+                            return View(filteredDataContext);
+                        }
+                    }
+                    else if (form["rateValueText"] != null)
+                    {
+                        var filteredDataContextWithCheckedAverageRate = workersList.Where(x => x.AverageRate >= minimumAverageRate).ToList();
+                        return View(filteredDataContextWithCheckedAverageRate);
+                    }
+                }
+                return View(workersList);
+            }
         }
 
         public ActionResult EmployeesFromProject(int projectId)
